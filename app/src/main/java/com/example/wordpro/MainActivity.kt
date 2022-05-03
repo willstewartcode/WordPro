95ef5b6b93eacbad4d6241d681e3733aab978530
Will Stewart <wstewart6@my.apsu.edu>
2022-05-03 11:03:02 -0400

    began adding code to switch to next edit text once a letter is put in

--- app/src/main/java/com/example/wordpro/MainActivity.kt
+++ app/src/main/java/com/example/wordpro/MainActivity.kt
@@ -2,7 +2,15 @@ package com.example.wordpro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import androidx.core.widget.addTextChangedListener
+import androidx.core.widget.doOnTextChanged
 import com.example.wordpro.databinding.ActivityMainBinding
+
+// Array that holds all of the edit texts that users put guesses into
+val GUESSBOXES = mutableListOf<EditText>()
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -10,5 +18,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val LINE1 = mutableListOf<EditText>(binding.edittextLine11, binding.edittextLine12,
+                                            binding.edittextLine13, binding.edittextLine14,
+                                            binding.edittextLine15)
+
+        LINE1.forEachIndexed { index, editText ->
+            if (index < LINE1.size) {
+                editText.addTextChangedListener {
+                    if (editText.text.toString().trim().length == 1) {
+                        LINE1[index + 1].requestFocus()
+                    }
+                }
+            }
+        }
     }
 }
