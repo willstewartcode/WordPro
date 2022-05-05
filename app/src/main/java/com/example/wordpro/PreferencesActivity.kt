50cb3c654de4132c555fef87da06c15ce4452889
Will Stewart <wstewart6@my.apsu.edu>
2022-05-04 23:15:43 -0400

    set up binding in preferences activity

--- app/src/main/java/com/example/wordpro/PreferencesActivity.kt
+++ app/src/main/java/com/example/wordpro/PreferencesActivity.kt
@@ -2,10 +2,13 @@ package com.example.wordpro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.wordpro.databinding.ActivityPreferencesBinding
 
 class PreferencesActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPreferencesBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_preferences)
+        binding = ActivityPreferencesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
