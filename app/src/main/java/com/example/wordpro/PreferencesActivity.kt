bdedd1fdb4415a930c334ef17a28cefe2fa0f312
Will Stewart <wstewart6@my.apsu.edu>
2022-05-04 23:48:43 -0400

    set up checkboxes in preferences activity

--- app/src/main/java/com/example/wordpro/PreferencesActivity.kt
+++ app/src/main/java/com/example/wordpro/PreferencesActivity.kt
@@ -1,14 +1,48 @@
 package com.example.wordpro
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CompoundButton
 import com.example.wordpro.databinding.ActivityPreferencesBinding
 
-class PreferencesActivity : AppCompatActivity() {
+class PreferencesActivity : AppCompatActivity(), CompoundButton.OnCheckedChangeListener {
     private lateinit var binding: ActivityPreferencesBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPreferencesBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.largetextCheckbox.setOnCheckedChangeListener(this)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val preferences = getSharedPreferences(
+            getString(R.string.preferences_storage_name),
+            Context.MODE_PRIVATE
+        )
+
+
+    }
+
+    override fun onCheckedChanged(checkbox: CompoundButton?, isChecked: Boolean) {
+        val preferences = getSharedPreferences(
+            getString(R.string.preferences_storage_name),
+            Context.MODE_PRIVATE
+        )
+
+        with(preferences.edit()) {
+            if (checkbox != null) {
+                if (checkbox.id == R.id.largetext_checkbox) {
+                    putBoolean(getString(R.string.text_size_key), isChecked)
+                    apply()
+                } else if (checkbox.id == R.id.use_alternative_colors_checkbox) {
+                    putBoolean(getString(R.string.alternative_colors_key), isChecked)
+                    apply()
+                }
+            }
+        }
     }
 }
